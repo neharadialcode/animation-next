0203ebaf9b16af616479453304460a8db85e9dbe
neharadialcode <neharadialcode@gmail.com>
2022-07-28 15:19:43 +0530

    animation modified

--- component/Animation.jsx
+++ component/Animation.jsx
@@ -17,6 +17,119 @@ const Animation = () => {
         pinSpacer: true,
       },
     });
+    let second = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".section_1",
+        start: "top top",
+        end: "bottom top",
+        scrub: 1,
+        immediateRender: false,
+      },
+    });
+    second
+      .fromTo(
+        ".img_one",
+        {
+          position: "absolute",
+          bottom: "-100vh",
+          left: "50%",
+          translateX: "-50%",
+          scale: 1.5,
+        },
+        {
+          position: "absolute",
+          bottom: "15vh",
+          right: 0,
+          scale: 1,
+        }
+      )
+      .to(".team", {
+        opacity: 0,
+      })
+      .fromTo(
+        ".img_two",
+        {
+          position: "absolute",
+          bottom: "-100vh",
+          left: "50%",
+          translateX: "-50%",
+          scale: 1.5,
+        },
+        {
+          position: "absolute",
+          bottom: "15vh",
+          right: 0,
+          scale: 1,
+        }
+      );
+    let third = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".section_2",
+        start: "top top",
+        end: "bottom top",
+        scrub: 1,
+        immediateRender: false,
+      },
+    });
+    third
+      .fromTo(
+        ".img_three",
+        {
+          position: "absolute",
+          bottom: "-100vh",
+          left: "50%",
+          translateX: "-50%",
+          scale: 1.5,
+        },
+        {
+          position: "absolute",
+          bottom: "15vh",
+          right: 0,
+          scale: 1,
+        }
+      )
+      .fromTo(
+        ".img_four",
+        {
+          position: "absolute",
+          bottom: "-100vh",
+          left: "50%",
+          translateX: "-50%",
+          scale: 1.5,
+        },
+        {
+          position: "absolute",
+          bottom: "15vh",
+          right: 0,
+          scale: 1,
+        }
+      );
+    let four = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".section_3",
+        start: "top top",
+        end: "bottom top",
+        scrub: 1,
+
+        immediateRender: false,
+      },
+    });
+    four.fromTo(
+      ".img_five",
+      {
+        position: "absolute",
+        bottom: "-100vh",
+        left: "50%",
+        translateX: "-50%",
+        scale: 1.5,
+      },
+      {
+        position: "absolute",
+        bottom: "15vh",
+        right: 0,
+        scale: 1,
+      }
+    );
 
     return () => {
       ScrollTrigger.getAll().forEach((instance) => {
@@ -32,17 +145,42 @@ const Animation = () => {
       </div>
       <div className="position-relative">
         <div className="container ">
-          <div className="row justify-content-between">
+          <div className="row justify-content-between section_1">
             <div className="col-6">
               <TextOne />
             </div>
-            <div className="col-5 d-flex flex-column justify-content-center target py-4 overflow-hidden">
-              <div className="image_conatiner mx-auto overflow-hidden ">
-                <img className="w-100" src="/img/one.png" alt="one" />
-                <img className="w-100" src="/img/two.png" alt="one" />
-                <img className="w-100" src="/img/three.png" alt="one" />
-                <img className="w-100" src="/img/four.png" alt="one" />
-                <img className="w-100" src="/img/five.png" alt="one" />
+            <div className="col-5 d-flex flex-column justify-content-center target py-4 ">
+              <div className="image_conatiner mx-auto overflow-hidden position-relative">
+                <img
+                  className="w-100 p-4 positoin-absolute team"
+                  src="/img/team.png"
+                  alt="team"
+                />
+                <img
+                  className="w-100 p-4 img_one"
+                  src="/img/one.png"
+                  alt="one"
+                />
+                <img
+                  className="w-100 p-4 img_two"
+                  src="/img/two.png"
+                  alt="one"
+                />
+                <img
+                  className="w-100 img_three p-4"
+                  src="/img/three.png"
+                  alt="one"
+                />
+                <img
+                  className="w-100 p-4 img_four"
+                  src="/img/four.png"
+                  alt="one"
+                />
+                <img
+                  className="w-100 p-4 img_five"
+                  src="/img/five.png"
+                  alt="one"
+                />
               </div>
             </div>
           </div>
