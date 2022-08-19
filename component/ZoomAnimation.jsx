aaefb47f1d5f67c8a9b056642a84bcf0a519c872
neharadialcode <neharadialcode@gmail.com>
2022-08-19 11:33:23 +0530

    btn three added

--- component/ZoomAnimation.jsx
+++ component/ZoomAnimation.jsx
@@ -4,13 +4,38 @@ import { useState } from "react";
 const ZoomAnimation = () => {
   const [img1, setImg1] = useState(false);
   const [img2, setImg2] = useState(false);
+  const [img3, setImg3] = useState(false);
   return (
     <div className="overflow-hidden">
-      <button className="btn_one" onClick={() => setImg1(!img1)}>
+      <button
+        className="btn_one"
+        onClick={() => {
+          setImg1(true);
+          setImg2(false);
+          setImg3(false);
+        }}
+      >
         one
       </button>
-      <button className="btn_two" onClick={() => setImg2(!img2)}>
+      <button
+        className="btn_two"
+        onClick={() => {
+          setImg1(false);
+          setImg2(true);
+          setImg3(false);
+        }}
+      >
         two
+      </button>
+      <button
+        className="btn_three"
+        onClick={() => {
+          setImg1(false);
+          setImg2(false);
+          setImg3(true);
+        }}
+      >
+        Three
       </button>
       <div className="position-relative">
         <div className="container ">
@@ -34,7 +59,22 @@ const ZoomAnimation = () => {
               <div className={` ${img2 ? "text_2_visible" : "text_2"}`}>
                 <div className="row">
                   <div className="col-4">
-                    <h1 className="text-white">hello</h1>
+                    <h1 className="text-white">This is second</h1>
+                  </div>
+                  <div className="col-8">
+                    <h2 className="text-white">
+                      Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                      Temporibus expedita perferendis ad eos et eveniet
+                      provident voluptas, libero ullam quo nisi vitae fugit fuga
+                      sit eum architecto iusto voluptatibus quisquam.
+                    </h2>
+                  </div>
+                </div>
+              </div>
+              <div className={` ${img3 ? "text_3_visible" : "text_3"}`}>
+                <div className="row">
+                  <div className="col-4">
+                    <h1 className="text-white">This is third</h1>
                   </div>
                   <div className="col-8">
                     <h2 className="text-white">
@@ -48,7 +88,13 @@ const ZoomAnimation = () => {
               </div>
               <div
                 className={`image_conatinerr mx-auto transition ${
-                  img1 ? "scale_img_one" : img2 ? "scale_img_two" : ""
+                  img1
+                    ? "scale_img_one"
+                    : img2
+                    ? "scale_img_two"
+                    : img3
+                    ? "scale_img_three"
+                    : ""
                 }`}
               >
                 <img
