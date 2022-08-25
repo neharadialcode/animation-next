13cd3779adf0a293e21cb2a8d751b3ffc02d86c6
neharadialcode <neharadialcode@gmail.com>
2022-08-25 10:12:54 +0530

    animation reverted

--- component/Animation.jsx
+++ component/Animation.jsx
@@ -6,93 +6,55 @@ import TextTwo from "./TextTwo";
 
 const Animation = () => {
   gsap.registerPlugin(ScrollTrigger);
-  gsap.defaults({ ease: "none", duration: 2 });
+  gsap.defaults({
+    ease: "none",
+    duration: 2,
+  });
   useLayoutEffect(() => {
     const tl = gsap.timeline();
-    // tl.fromTo(
-    //   ".onee",
-    //   {
-    //     xPercent: 0,
-    //   },
-    //   {
-    //     xPercent: 0,
-    //   }
-    // )
-    //   .fromTo(
-    //     ".twoo",
-    //     {
-    //       xPercent: 100,
-    //       zIndex: 1,
-    //     },
-    //     {
-    //       xPercent: 0,
-    //     }
-    //   )
-    //   .fromTo(
-    //     ".threee",
-    //     {
-    //       yPercent: -100,
-    //       zIndex: 2,
-    //     },
-    //     {
-    //       yPercent: 0,
-    //     }
-    //   )
-    //   .fromTo(
-    //     ".fourr",
-    //     {
-    //       yPercent: 100,
-    //       zIndex: 3,
-    //     },
-    //     {
-    //       yPercent: 0,
-    //     }
-    //   );
-
-    let six = gsap.timeline({
-      scrollTrigger: {
-        trigger: ".onee",
-        start: "top top",
-        end: "bottom top",
-        scrub: 1,
-        markers: true,
+    tl.fromTo(
+      ".onee",
+      {
+        xPercent: 0,
       },
-    });
-    six.from(".onee", {
-      xPercent: -100,
-    });
-    let twoo = gsap.timeline({
-      scrollTrigger: {
-        trigger: ".twoo",
-        start: "top top",
-        end: "bottom top",
-        scrub: 1,
-      },
-    });
-    twoo.from(".twoo", { xPercent: -100, zIndex: 1 });
-    let threee = gsap.timeline({
-      scrollTrigger: {
-        trigger: ".threee",
-        start: "top top",
-        end: "bottom top",
-        scrub: 1,
-      },
-    });
-    threee.from(".threee", { xPercent: -100, zIndex: 2 });
-    let fourr = gsap.timeline({
-      scrollTrigger: {
-        trigger: ".fourr",
-        start: "top top",
-        end: "bottom bottom",
-        scrub: 1,
-      },
-    });
-    fourr.from(".fourr", { xPercent: -100, zIndex: 3 });
+      {
+        xPercent: 0,
+      }
+    )
+      .fromTo(
+        ".twoo",
+        {
+          xPercent: 100,
+          zIndex: 1,
+        },
+        {
+          xPercent: 0,
+        }
+      )
+      .fromTo(
+        ".threee",
+        {
+          yPercent: -100,
+          zIndex: 2,
+        },
+        {
+          yPercent: 0,
+        }
+      )
+      .fromTo(
+        ".fourr",
+        {
+          yPercent: 100,
+          zIndex: 3,
+        },
+        {
+          yPercent: 0,
+        }
+      );
   }, []);
 
   return (
-    <div className="  position-relative main_parent ">
-      <div className="vh-100"></div>
+    <div className="overflow-hidden vh-100 position-relative">
       <div className="bg-success vh-100 d-flex onee flex-column justify-content-center align-items-center position-absolute w-100">
         <h1 className="text-white fw-bold">one</h1>
       </div>
@@ -105,9 +67,9 @@ const Animation = () => {
       <div className="bg-danger fourr vh-100 d-flex flex-column justify-content-center align-items-center position-absolute w-100">
         <h1 className="text-white fw-bold">four</h1>
       </div>
-      {/* <div className="bg-dark vh-100 d-flex flex-column justify-content-center align-items-center">
+      <div className="bg-dark vh-100 d-flex flex-column justify-content-center align-items-center">
         <h1 className="text-white fw-bold">five</h1>
-      </div> */}
+      </div>
     </div>
   );
 };
